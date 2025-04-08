3a8f6f8a0777bf83eeb1add31f680b9f3e0a9df2
Lucassassis <Lucassassis@live.com>
2025-04-07 23:20:45 -0300

    Ini Js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+    addBtn = document.getElementById("add-btn");
+    popup = document.getElementById("popup");
 
+    addBtn.addEventListener("click", () => {
+        popup.style.display = "flex";
+
+     });
 })
